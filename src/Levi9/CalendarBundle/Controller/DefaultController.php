284feeb2da1042e9e1d79a33a097d95eb65bbb9c
Nikolay Labinskiy <e-moe@ukr.net>
2014-09-07 12:01:14 +0300

    login

--- src/Levi9/CalendarBundle/Controller/DefaultController.php
+++ src/Levi9/CalendarBundle/Controller/DefaultController.php
@@ -3,9 +3,13 @@
 namespace Levi9\CalendarBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Security("has_role('ROLE_USER')")
+     */
     public function indexAction()
     {
         $calendarResults = $this->get('levi9_calendar.calendar')->getListData();
