e4620eaafb7ee715165e66a0e1d43b609cf4219d
Nikolay Labinskiy <e-moe@ukr.net>
2014-09-07 15:18:12 +0300

    route annotation

--- src/Levi9/CalendarBundle/Controller/DefaultController.php
+++ src/Levi9/CalendarBundle/Controller/DefaultController.php
@@ -4,11 +4,18 @@ namespace Levi9\CalendarBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
     /**
      * @Security("has_role('ROLE_USER')")
+     * @Route(
+     *      "/{_locale}",
+     *      name="calendar_index",
+     *      requirements={"_locale" = "en|ru"},
+     *      defaults={"_locale" = "en"}
+     *  )
      */
     public function indexAction()
     {
